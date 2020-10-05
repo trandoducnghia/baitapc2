1b2a2bd494179e2fdb407c881904e075ab9e743f
trandoducnghia <trandoducnghia@gmail.com>
2020-10-05 15:28:24 +0700

    Create ai3.cpp

---
+++ ai3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    float n;
+    int i;
+    cout << "Vui long nhapN";
+    cin >> n;
+    for(i=0;i<=n;i++)
+    {
+        if(i%2 !=0) cout << i << "\n";
+
+    }
+
+
+    return 0;
+}
